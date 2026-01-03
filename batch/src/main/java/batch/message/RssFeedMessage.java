hj0328/techgather-backend-setting-cicd

feat: Implement batch job for collecting RSS feeds #4

batch/src/main/java/batch/message/RssFeedMessage.java
@@ -1,0 +1,13 @@
+package batch.message;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record RssFeedMessage(String title,
+							 String url,
+							 LocalDateTime pubDate,
+							 List<String> tags,
+							 String description,
+							 String thumbnail) {
+
+}

techgather/techgather-backend

batch/src/main/java/batch/message/RssFeedMessage.java
@@ -1,0 +1,13 @@
+package batch.message;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record RssFeedMessage(String title,
+							 String url,
+							 LocalDateTime pubDate,
+							 Set<String> tags,
+							 String description,
+							 String thumbnail) {
+
+}